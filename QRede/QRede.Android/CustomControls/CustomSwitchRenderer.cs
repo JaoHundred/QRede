4797ac3f9a2baeb0c26ba73fc63911e75ba90464
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2020-03-18 17:44:40 -0300

    Parcial, implementado cores do switch no modo claro e criado custom renderer para o switch, permitindo a exposição da propriedade de OffColor.

---
+++ QRede/QRede.Android/CustomControls/CustomSwitchRenderer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Graphics;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using QRede.CustomControls;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(CustomSwitch)
+    , typeof(QRede.Droid.CustomControls.CustomSwitchRenderer))]
+namespace QRede.Droid.CustomControls
+{
+    public class CustomSwitchRenderer : SwitchRenderer
+    {
+        public CustomSwitchRenderer(Context context) : base(context)
+        { }
+
+        private CustomSwitch view;
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Switch> e)
+        {
+            base.OnElementChanged(e);
+            if (e.OldElement != null || e.NewElement == null)
+                return;
+            view = (CustomSwitch)Element;
+            if (Android.OS.Build.VERSION.SdkInt >= Android.OS.BuildVersionCodes.JellyBean)
+            {
+                if (this.Control != null)
+                {
+                    if (this.Control.Checked)
+                    {
+                        this.Control.TrackDrawable.SetColorFilter(view.OnColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
+                    }
+                    else
+                    {
+                        this.Control.TrackDrawable.SetColorFilter(view.OffColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
+                    }
+                    this.Control.CheckedChange += this.OnCheckedChange;
+                }
+            }
+        }
+
+        private void OnCheckedChange(object sender, CompoundButton.CheckedChangeEventArgs e)
+        {
+            if (this.Control.Checked)
+            {
+                this.Control.TrackDrawable.SetColorFilter(view.OnColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
+            }
+            else
+            {
+                this.Control.TrackDrawable.SetColorFilter(view.OffColor.ToAndroid(), PorterDuff.Mode.SrcAtop);
+            }
+
+            Element.IsToggled = Control.Checked;
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            this.Control.CheckedChange -= this.OnCheckedChange;
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ QRede/QRede/CustomControls/CustomSwitch.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace QRede.CustomControls
+{
+    public class CustomSwitch : Switch
+    {
+
+        //TODO: https://github.com/JaoHundred/QRede/issues/13
+
+        public static readonly BindableProperty SwitchOffColorProperty =
+     BindableProperty.Create(nameof(OffColor),
+         typeof(Color), typeof(CustomSwitch),
+         Color.Green);
+
+        public Color OffColor
+        {
+            get { return (Color)GetValue(SwitchOffColorProperty); }
+            set { SetValue(SwitchOffColorProperty, value); }
+        }
+    }
+}
