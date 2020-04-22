cbd9bc51fb0ac002fc8cb069164e429dd03ae8e4
xhugo92 <hugofrn1992@hotmail.com>
2020-04-22 17:55:46 -0300

    Implementado a visualização da Collection de Qr Codes Design da lista

---
+++ QRede/QRede/Modules/QRCollection/QRCollectionView.xaml.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace QRede.Modules
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class QRCollectionView : ContentPage
+    {
+        public QRCollectionView()
+        {
+            InitializeComponent();
+            BindingContext = new QRCollectionViewModel();
+        }
+    }
+}
---
+++ QRede/QRede/Modules/QRCollection/QRCollectionViewModel.cs
@@ -0,0 +1,32 @@
+﻿using QRede.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using QRede.Interfaces;
+using MvvmHelpers;
+using System.Threading.Tasks;
+
+namespace QRede.Modules
+{
+    class QRCollectionViewModel : IAsyncInitialization
+    {
+        public QRCollectionViewModel()
+        {
+            WifiSummaryCollection = new ObservableRangeCollection<WifiSummary>();
+            LoadTask = LoadAsync();
+        }
+
+        public ObservableRangeCollection<WifiSummary> WifiSummaryCollection { get; set; }
+
+        public Task LoadTask { get; }
+
+        public async Task LoadAsync()
+        {
+            await Task.Run(() =>
+            {
+                WifiSummaryCollection.AddRange(App.WifiSummaryCollection);
+            });
+        }
+    }
+}
