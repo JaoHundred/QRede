b1b1f420fe5420e1318f590beee5443198b63c48
xhugo92 <hugofrn1992@hotmail.com>
2020-05-16 15:32:08 -0300

    Botão de delete do qrcode funcional

--- QRede/QRede/Modules/QRCollection/QRCollectionViewModel.cs
+++ QRede/QRede/Modules/QRCollection/QRCollectionViewModel.cs
@@ -5,7 +5,10 @@ using System.Collections.ObjectModel;
 using System.Text;
 using QRede.Interfaces;
 using MvvmHelpers;
+using MvvmHelpers.Commands;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using QRede.Services;
 
 namespace QRede.Modules
 {
@@ -14,9 +17,20 @@ namespace QRede.Modules
         public QRCollectionViewModel()
         {
             WifiSummaryCollection = new ObservableRangeCollection<WifiSummary>();
+            DeleteCommand = new AsyncCommand<WifiSummary>(OnDelete);
         }
 
         public ObservableRangeCollection<WifiSummary> WifiSummaryCollection { get; set; }
+
+        public ICommand DeleteCommand { get; set; }
+
+        private async Task OnDelete(WifiSummary wifiSummary)
+        {
+            WifiSummaryCollection.Remove(wifiSummary);
+            App.WifiSummaryCollection.Remove(wifiSummary);
+            await IOService<List<WifiSummary>>.WriteAsync(App.WifiSummaryCollection, Constants.QrCodeFilePath);
+
+        }
 
         public async Task LoadAsync()
         {
