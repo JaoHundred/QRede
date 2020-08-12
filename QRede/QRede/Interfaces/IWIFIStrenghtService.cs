fa3d51f8d1044c3009ab45ce648ab1b9fc7a859a
JaoHundred <43298295+JaoHundred@users.noreply.github.com>
2020-08-12 17:37:40 -0300

    Implementado níveis de sinal no módulo Home.

---
+++ QRede/QRede/Interfaces/IWIFIStrenghtService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QRede.Interfaces
+{
+    public interface IWIFIStrenghtService
+    {
+        int CalculateStrenght();
+    }
+}
