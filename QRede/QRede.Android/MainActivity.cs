abd1828bb9813126b1f659fa39b763c2d18ac1ea
xhugo92 <hugofrn1992@hotmail.com>
2020-09-09 16:42:10 -0300

    Adicionado SplashScreen

--- QRede/QRede.Android/MainActivity.cs
+++ QRede/QRede.Android/MainActivity.cs
@@ -9,7 +9,8 @@ using FFImageLoading;
 
 namespace QRede.Droid
 {
-    [Activity(Label = "QRede", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Theme = "@style/MainTheme",  ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation
+        , ScreenOrientation= ScreenOrientation.Portrait)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
---
+++ QRede/QRede.Android/SplashActivity.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Android.Runtime;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Widget;
+
+namespace QRede.Droid
+{
+    [Activity(Label = "QRede", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, Icon = "@mipmap/icon",
+        Theme= "@style/MyTheme.Splash")]
+    public class SplashActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+            OverridePendingTransition(0, 0);
+            Finish();
+        }
+
+ 
+    }
+}
